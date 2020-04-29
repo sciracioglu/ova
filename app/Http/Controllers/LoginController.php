73c169b6fac92bb2aa757de6e3ae4123d65ecd09
Serkan Ciracioglu <sc@Serkans-MacBook-Pro.local>
2020-04-29 19:19:46 +0300

    input sil

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -1,78 +1,79 @@
-<?php namespace App\Http\Controllers;
+<?php
 
-use App\Http\Requests\LoginRequest;
+namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Session;
 use Gregwar\Captcha\CaptchaBuilder;
 
-class LoginController extends Controller {
+class LoginController extends Controller
+{
+    public function guvenlikKodu()
+    {
+        $builder = new CaptchaBuilder();
+        $builder->build();
+        session()->put('phrase', $builder->getPhrase());
 
-	function guvenlikKodu(){
-		$builder = new CaptchaBuilder();
-		$builder->build();
-		session()->put('phrase',$builder->getPhrase());
+        return $builder->inline();
+    }
 
-		return $builder->inline();
-	}
+    //
+    public function getLogin()
+    {
+        $metin = $this->guvenlikKodu();
 
-	//
-	public function getLogin(){
+        return view('login.login', compact('metin'));
+    }
 
-		$metin = $this->guvenlikKodu();
+    public function postLogin(Request $request)
+    {
+        $this->validate($request, [
+            'kullanici' => 'required',
+            'sifre' => 'required|min:4',
+            'gkod' => 'required|min:3'
+        ]);
+        if (session('phrase') != $request->get('gkod')) {
+            return redirect('/login')->with('warning', 'Güvenlik kodu hatalı!');
+        }
 
+        $kullanicilar = DB::select('select dbo.CheckPassword(?,WEBPASSWORD) AS SAYI FROM  vwwusr WHERE USERNAME =?', [
+            $request->get('sifre'),
+            $request->get('kullanici')
+        ]);
 
-		return view('login.login', compact('metin'));
-	}
+        if (!$kullanicilar) {
+            return Redirect::to('login')->with('warning', 'Hatali bilgi girdiniz!');
+        }
 
-	public function postLogin(Request $request){
-		$this->validate($request, [
-			'kullanici' => 'required',
-			'sifre'     => 'required|min:4',
-			'gkod'      => 'required|min:3'
-		]);
-		if(session('phrase') != $request->get('gkod')){
-			return redirect('/login')->with('warning', 'Güvenlik kodu hatalı!');
-		}
+        $eposta = DB::select('SELECT EMAIL FROM vwwusr WHERE USERNAME	 =?', [
+            $request->get('kullanici')
+        ]);
 
-		$kullanicilar = DB::select("select dbo.CheckPassword(?,WEBPASSWORD) AS SAYI FROM  vwwusr WHERE USERNAME =?", [
-			$request->get('sifre'),
-			$request->get('kullanici')
-		]);
-
-		if(!$kullanicilar){
-			return Redirect::to('login')->with('warning', 'Hatali bilgi girdiniz!');
-		}
-
-		$eposta = DB::select("SELECT EMAIL FROM vwwusr WHERE USERNAME	 =?",[
-			$request->get('kullanici')
-		]);
-
-		$tip = DB::select("SELECT KOD, SUBSTRING( dbo.refkrt.ACIKLAMA,5,100) ACIKLAMA FROM refkrt
+        $tip = DB::select("SELECT KOD, SUBSTRING( dbo.refkrt.ACIKLAMA,5,100) ACIKLAMA FROM refkrt
 							WHERE dbo.refkrt.ALANAD	='EVRAKTIP' AND dbo.refkrt.TABLOAD	='EVRBAS'");
-		$evraktip = [];
-		foreach($tip as $t){
-			$evraktip[$t->KOD] = $t->ACIKLAMA;
-		}
-		Session::put('evraktip',$evraktip);
+        $evraktip = [];
+        foreach ($tip as $t) {
+            $evraktip[$t->KOD] = $t->ACIKLAMA;
+        }
+        Session::put('evraktip', $evraktip);
 
-		foreach($kullanicilar as $k){
-			if($k->SAYI == 1){
-				session()->put('kullanici.username', Input::get('kullanici'));
+        foreach ($kullanicilar as $k) {
+            if ($k->SAYI == 1) {
+                session()->put('kullanici.username', Input::get('kullanici'));
 
-				return Redirect::to('/');
-			}
-		}
+                return Redirect::to('/');
+            }
+        }
 
-		return Redirect::to('login')->with('warning', 'Hatalı bilgi girdiniz!');
-	}
+        return Redirect::to('login')->with('warning', 'Hatalı bilgi girdiniz!');
+    }
 
-	public function logout(){
-		Session::flush();
+    public function logout()
+    {
+        Session::flush();
 
-		return Redirect::to('/login');
-	}
-
+        return Redirect::to('/login');
+    }
 }
