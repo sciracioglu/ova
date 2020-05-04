a9deaac326b2f4cb85750a23f516bc65bda78538
Serkan Ciracioglu <sc@Serkans-MacBook-Pro.local>
2020-05-04 21:12:13 +0300

    ..

--- resources/views/tahsilat.blade.php
+++ resources/views/tahsilat.blade.php
@@ -1,107 +1,129 @@
 @extends('layouts.master')
 
+
 @section('title')
-    Tahsilat
-@endsection
+   Tahsilat
+@stop
+
 
 @section('content')
+<style>
+.temel:hover{
+   background: -moz-linear-gradient(top, rgba(161,219,255,0.6) 0%, rgba(161,219,255,0.6) 100%);
+background: -webkit-linear-gradient(top, rgba(161,219,255,0.6) 0%,rgba(161,219,255,0.6) 100%);
+background: linear-gradient(to bottom, rgba(161,219,255,0.6) 0%,rgba(161,219,255,0.6) 100%);
+filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#99a1dbff', endColorstr='#99a1dbff',GradientType=0 );
+}
+</style>
 <div id='app'>
-    <div class="list-group">
-        <a href='#ay' @click='yilAta(yil.yil)' class="list-group-item  " :class="[isYear(yil) ? 'active' :'']" v-for='yil in yillar'>
-            <div class="row">
-                <div class="col-md-4">@{{ yil.yil }}</div>
-                <div class="col-md-4" v-text='format(yil.evrak_tutar,"Evrak Tutar : ")'></div>
-                <div class="col-md-4" v-text='format(yil.tutar,"Tutar : ")'></div>
+   <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+      <div class="panel panel-default" v-for='yil in yillar'>
+        <div class="panel-heading temel" role="tab" id="headingOne">
+          <h4 class="panel-title">
+            <a role="button" data-toggle="collapse" :href="'#a'+yil.yil" aria-expanded="true" aria-controls="collapseOne">
+               <div class="row">
+                  <div class="col-md-4"> @{{ yil.yil }}</div>
+                  <div class="col-md-4" style="text-align:right;">
+                     <span v-text='format(yil.evrak_tutar,"Evrak Tutar : ")'></span>
+                  </div>
+                  <div class="col-md-4" style="text-align:right;">
+                     <span v-text='format(yil.tutar,"Tutar : ")'></span>
+                  </div>
+               </div>
+            </a>
+          </h4>
+        </div>
+        <div :id="'a'+yil.EVRAKYIL" class="panel-collapse collapse" role="tabpanel">
+          <div class="panel-body">
+            <div class="panel-group" :id="'ay'+yil.yil" role="tablist" aria-multiselectable="true">
+
+               <div class="panel panel-default" v-for='ay in aylar'  v-if='ay.yil == yil.yil'>
+                 <div class="panel-heading temel" role="tab" id="headingOne">
+                   <h4 class="panel-title">
+                     <a role="button" data-toggle="collapse" :data-parent="'#ay'+yil.yil" :href="'#b'+yil.yil+ay.ay" aria-expanded="true" aria-controls="collapseOne">
+                           <div class="row">
+                                 <div class="col-md-4">  @{{ ay.ay }}</div>
+                                 <div class="col-md-4" style="text-align:right;">
+                                       <span v-text='format(ay.evrak_tutar,"Evrak Tutar : ")'></span>
+                                 </div>
+                                 <div class="col-md-4" style="text-align:right;">
+                                       <span v-text='format(ay.tutar,"Tutar : ")'></span>
+                                 </div>
+                              </div>
+                     </a>
+                   </h4>
+                 </div>
+                 <div :id="'b'+yil.yil+ay.ay" class="panel-collapse collapse" role="tabpanel">
+                   <div class="panel-body">
+                      <table class="table table-condenced table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Evrak Tipi</th>
+                                    <th>Evrak Tarihi</th>
+                                    <th>Hesap Kodu</th>
+                                    <th>Ünvanı</th>
+                                    <th>Karşı Hesap Kodu</th>
+                                    <th>Karşı Hesap Kart Ünvan</th>
+                                    <th>Evrak Tutar</th>
+                                    <th>Evrak Döviz Cinsi</th>
+                                    <th>Evrak Döviz Kuru</th>
+                                    <th>Tutar</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-if='yil == tahsilat._EVRAKYIL && ay == tahsilat._EVRAKAY' v-for='tahsilat in tahsilatlar'>
+                                    <td>@{{ tahsilat.EVRAKTIP }}</td>
+                                    <td>@{{ tahsilat.EVRAKTARIH }}</td>
+                                    <td>@{{ tahsilat.HESAPKOD }}</td>
+                                    <td>@{{ tahsilat.CARKRT_UNVAN }}</td>
+                                    <td>@{{ tahsilat.KARSIHESAPKOD }}</td>
+                                    <td>@{{ tahsilat.CRKKRS_UNVAN }}</td>
+                                    <td>@{{ tahsilat.EVRAKTUTAR }}</td>
+                                    <td>@{{ tahsilat.EVRAKDOVIZCINS }}</td>
+                                    <td>@{{ tahsilat.EVRAKDOVIZKUR }}</td>
+                                    <td>@{{ tahsilat.TUTAR }}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                   </div>
+                 </div>
+               </div>
+
             </div>
-            <div class="row" id='ay'>
-                <div class="col-md-12" v-if='yil == yil.yil'>
-                    <div class="list-group">
-                        <a href='#detay' @click='ayAta(ay.ay)' class="list-group-item " :class="[isMonth(ay) ? 'active' :'']" v-if='yil == ay.yil'  v-for='ay in aylar'>
-                            <div class="row">
-                                <div class="col-md-4">@{{ ay.ay }}</div>
-                                <div class="col-md-4" v-text='format(ay.evrak_tutar,"Evrak Tutar : ")'></div>
-                                <div class="col-md-4" v-text='format(ay.tutar,"Tutar : ")'></div>
-                            </div>
-                            <div class="row" v-if='yil == ay.yil && ay == ay.ay'>
-                                <div class="col-md-12">
-                                    <div class="list-group" id='detay'>
-                                        <table class="table table-condenced table-hover">
-                                            <thead>
-                                                <tr>
-                                                    <th>Evrak Tipi</th>
-                                                    <th>Evrak Tarihi</th>
-                                                    <th>Hesap Kodu</th>
-                                                    <th>Ünvanı</th>
-                                                    <th>Karşı Hesap Kodu</th>
-                                                    <th>Karşı Hesap Kart Ünvan</th>
-                                                    <th>Evrak Tutar</th>
-                                                    <th>Evrak Döviz Cinsi</th>
-                                                    <th>Evrak Döviz Kuru</th>
-                                                    <th>Tutar</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                <tr v-if='tahsilat._EVRAKYIL == yil && tahsilat._EVRAKAY == ay' v-for='tahsilat in tahsilatlar'>
-                                                    <td>@{{ tahsilat.EVRAKTIP }}</td>
-                                                    <td>@{{ tahsilat.EVRAKTARIH }}</td>
-                                                    <td>@{{ tahsilat.HESAPKOD }}</td>
-                                                    <td>@{{ tahsilat.CARKRT_UNVAN }}</td>
-                                                    <td>@{{ tahsilat.KARSIHESAPKOD }}</td>
-                                                    <td>@{{ tahsilat.CRKKRS_UNVAN }}</td>
-                                                    <td>@{{ tahsilat.EVRAKTUTAR }}</td>
-                                                    <td>@{{ tahsilat.EVRAKDOVIZCINS }}</td>
-                                                    <td>@{{ tahsilat.EVRAKDOVIZKUR }}</td>
-                                                    <td>@{{ tahsilat.TUTAR }}</td>
-                                                </tr>
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </a>
-    </div>
+          </div>
+        </div>
+      </div>
+
+   </div>
 </div>
+
+
 @endsection
+
 @section('scripts')
-<script src="//cdnjs.cloudflare.com/ajax/libs/numeral.js/2.0.6/numeral.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/vue@2.6.11"></script>
 <script>
 var vue = new Vue({
-    el:'#app',
-    data:{
-        yil:'',
-        ay:'',
-        yillar:{!! $yillar !!},
-        aylar:{!! $aylar !!},
-        tahsilatlar:{!! $tahsilatlar !!},
-    },
-    methods:{
-        format(rakam,title){
-                return  title + ' ' + numeral(rakam) . format('0,0.00');
-        },
-        isYear(yil){
-            if(this.yil == yil.yil){
-                return true;
-            }
-            return false;
-        },
-        isMonth(yil, ay){
-            if(this.yil == ay.yil && this.ay == ay.ay){
-                return true;
-            }
-            return false;
-        },
-        yilAta(yil){
-            this.yil = yil;
-        },
-        ayAta(ay){
-            this.ay = ay;
-        }
-    }
-});
+   el:'#app',
+   data:{
+      tahsilatlar:{!! $tahsilatlar !!},
+      yillar:{!! $yillar !!},
+      aylar:{!! $aylar !!},
+      tipler:{!! $tipler !!},
+      yil:null,
+      ay:null,
+   },
+   methods:{
+      url(id){
+         return '#a'+id;
+      },
+      format(fiyat,msg,ust=0){
+         if(ust ==1){
+            return msg+": "+ parseFloat(fiyat).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+         }
+         return parseFloat(fiyat).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+      },
+   }
+})
 </script>
 @endsection
