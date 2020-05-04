c4e372518d8de17eddfcb58082c15a746ea7f759
Serkan Ciracioglu <sc@Serkans-MacBook-Pro.local>
2020-05-04 13:21:53 +0300

    ..

--- app/Http/Controllers/TahsilatController.php
+++ app/Http/Controllers/TahsilatController.php
@@ -8,7 +8,19 @@ class TahsilatController extends Controller
 {
     public function index()
     {
-        $tahsilatlar = DB::select('EXEC [dbo].[spArgWebTahsilatRapor] ?', [session('musteri.hesapkod')]);
-        dd($tahsilatlar);
+        $tahsilatlar = collect(DB::select('EXEC [dbo].[spArgWebTahsilatRapor] ?', [session('musteri.hesapkod')]));
+        $evrak_tutar = 0;
+        $tutar = 0;
+        $tasilat_yil = $tahsilatlar->groupBy('EVRAKYIL')
+                                ->each(function ($tahsilat) use ($evrak_tutar,$tutar) {
+                                    $evrak_tutar += $tahsilat->EVRAKTUTAR;
+                                    $tutar += $tahsilat->TUTAR;
+                                })
+                                ->map(function ($tahsilat) use ($evrak_tutar, $tutar) {
+                                    return ['yil' => $tahsilat->_EVRAKYIL,
+                                        'evrak_tutar' => $evrak_tutar,
+                                        'tutar' => $tutar];
+                                });
+        dd($tasilat_yil);
     }
 }
