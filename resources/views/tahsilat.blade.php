019fddb9dfaf75a1f7c1d2eb2cbfec2c0b8c775e
Serkan Ciracioglu <sc@Serkans-MacBook-Pro.local>
2020-05-04 20:43:10 +0300

    ..

--- resources/views/tahsilat.blade.php
+++ resources/views/tahsilat.blade.php
@@ -13,47 +13,51 @@
                 <div class="col-md-4" v-text='format(yil.evrak_tutar,"Evrak Tutar : ")'></div>
                 <div class="col-md-4" v-text='format(yil.tutar,"Tutar : ")'></div>
             </div>
-            <div class="list-group" v-if='yil == yil.yil' id='ay'>
-                <a href='#detay' @click='ayAta(ay.ay)' class="list-group-item list-group-item-action" :class="[isMonth(ay) ? 'active' :'']"  v-for='ay in aylar'>
-                    <div class="row">
-                        <div class="col-md-4">@{{ ay.ay }}</div>
-                        <div class="col-md-4" v-text='format(ay.evrak_tutar,"Evrak Tutar : ")'></div>
-                        <div class="col-md-4" v-text='format(ay.tutar,"Tutar : ")'></div>
+            <div class="row" v-if='yil == yil.yil' id='ay'>
+                <div class="col-md-12">
+                    <div class="list-group">
+                        <a href='#detay' @click='ayAta(ay.ay)' class="list-group-item list-group-item-action" :class="[isMonth(ay) ? 'active' :'']"  v-for='ay in aylar'>
+                            <div class="row">
+                                <div class="col-md-4">@{{ ay.ay }}</div>
+                                <div class="col-md-4" v-text='format(ay.evrak_tutar,"Evrak Tutar : ")'></div>
+                                <div class="col-md-4" v-text='format(ay.tutar,"Tutar : ")'></div>
+                            </div>
+                            <div class="list-group" id='detay'>
+                                <table class="table table-condenced table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th>Evrak Tipi</th>
+                                            <th>Evrak Tarihi</th>
+                                            <th>Hesap Kodu</th>
+                                            <th>Ünvanı</th>
+                                            <th>Karşı Hesap Kodu</th>
+                                            <th>Karşı Hesap Kart Ünvan</th>
+                                            <th>Evrak Tutar</th>
+                                            <th>Evrak Döviz Cinsi</th>
+                                            <th>Evrak Döviz Kuru</th>
+                                            <th>Tutar</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr v-if='tahsilat._EVRAKYIL == yil && tahsilat._EVRAKAY == ay' v-for='tahsilat in tahsilatlar'>
+                                            <td>@{{ tahsilat.EVRAKTIP }}</td>
+                                            <td>@{{ tahsilat.EVRAKTARIH }}</td>
+                                            <td>@{{ tahsilat.HESAPKOD }}</td>
+                                            <td>@{{ tahsilat.CARKRT_UNVAN }}</td>
+                                            <td>@{{ tahsilat.KARSIHESAPKOD }}</td>
+                                            <td>@{{ tahsilat.CRKKRS_UNVAN }}</td>
+                                            <td>@{{ tahsilat.EVRAKTUTAR }}</td>
+                                            <td>@{{ tahsilat.EVRAKDOVIZCINS }}</td>
+                                            <td>@{{ tahsilat.EVRAKDOVIZKUR }}</td>
+                                            <td>@{{ tahsilat.TUTAR }}</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </a>
                     </div>
-                    <div class="list-group" id='detay'>
-                        <table class="table table-condenced table-hover">
-                            <thead>
-                                <tr>
-                                    <th>Evrak Tipi</th>
-                                    <th>Evrak Tarihi</th>
-                                    <th>Hesap Kodu</th>
-                                    <th>Ünvanı</th>
-                                    <th>Karşı Hesap Kodu</th>
-                                    <th>Karşı Hesap Kart Ünvan</th>
-                                    <th>Evrak Tutar</th>
-                                    <th>Evrak Döviz Cinsi</th>
-                                    <th>Evrak Döviz Kuru</th>
-                                    <th>Tutar</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr v-if='tahsilat._EVRAKYIL == yil && tahsilat._EVRAKAY == ay' v-for='tahsilat in tahsilatlar'>
-                                    <td>@{{ tahsilat.EVRAKTIP }}</td>
-                                    <td>@{{ tahsilat.EVRAKTARIH }}</td>
-                                    <td>@{{ tahsilat.HESAPKOD }}</td>
-                                    <td>@{{ tahsilat.CARKRT_UNVAN }}</td>
-                                    <td>@{{ tahsilat.KARSIHESAPKOD }}</td>
-                                    <td>@{{ tahsilat.CRKKRS_UNVAN }}</td>
-                                    <td>@{{ tahsilat.EVRAKTUTAR }}</td>
-                                    <td>@{{ tahsilat.EVRAKDOVIZCINS }}</td>
-                                    <td>@{{ tahsilat.EVRAKDOVIZKUR }}</td>
-                                    <td>@{{ tahsilat.TUTAR }}</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </a>
-            </>
+                </div>
+            </div>
         </a>
     </div>
 </div>
