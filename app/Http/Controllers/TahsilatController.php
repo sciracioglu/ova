5df08af9b351e57b6d36cfb86b53b80bf5b1ecb6
Serkan Ciracioglu <sc@Serkans-MacBook-Pro.local>
2020-05-04 14:13:09 +0300

    ..

--- app/Http/Controllers/TahsilatController.php
+++ app/Http/Controllers/TahsilatController.php
@@ -12,32 +12,10 @@ class TahsilatController extends Controller
         $yillar = Tahsilat::where('HESAPKOD', [session('musteri.hesapkod')])
                             ->groupBy('_EVRAKYIL')
                             ->get(DB::raw('_EVRAKYIL as yil, sum(EVRAKTUTAR) as evrak_tutuar, sum(TUTAR) as tutar'));
+
         $aylar = Tahsilat::where('HESAPKOD', [session('musteri.hesapkod')])
                             ->groupBy('_EVRAKYIL', '_EVRAKAY')
-                            ->get(DB::raw('_EVRAKYIL as yil, sum(EVRAKTUTAR) as evrak_tutuar, sum(TUTAR) as tutar'));
+                            ->get(DB::raw('_EVRAKYIL as yil, _EVRAKAY as ay, sum(EVRAKTUTAR) as evrak_tutuar, sum(TUTAR) as tutar'));
         dd($aylar);
-        $tasilat_kayit = collect($tahsilatlar->map(function ($tahsilat) {
-            return [
-                'yil' => $tahsilat->_EVRAKYIL,
-                'ay' => $tahsilat->_EVRAKAY,
-                'evrak_tutar' => $tahsilat->EVRAKTUTAR,
-                'tutar' => $tahsilat->TUTAR,
-                'evrakno' => $tahsilat->EVRAKNO,
-                'evrak_tarih' => $tahsilat->EVRAKTARIH,
-                'hesapkod' => $tahsilat->HESAPKOD,
-                'unvan' => $tahsilat->CARKRT_UNVAN,
-                'karsi_hesap' => $tahsilat->KARSIHESAPKOD,
-                'karsi_unvan' => $tahsilat->CRKKRS_UNVAN,
-                'doviz' => isset($tahsilat->EVRAKDOVIZCINSI) ? $tahsilat->EVRAKDOVIZCINSI : '',
-                'kur' => $tahsilat->EVRAKDOVIZKUR,
-                'evrak_tip' => $tahsilat->EVRAKTIP,
-                'aciklama' => $tahsilat->ACIKLAMA1,
-            ];
-        }));
-        $yillar = $tasilat_kayit->groupBy('yil');
-        dump($yillar);
-        $aylar = $yillar->groupBy('ay');
-        dd($aylar);
-        dd($tasilat_kayit->groupBy('yil')->sum('evrak_tutar'));
     }
 }
